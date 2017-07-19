9841695844c9baecf07ba001562d0247858afc2a
李杨 <liyang@liyangdeMacBook-Pro.local>
2017-07-19 14:57:44 +0800

    refs

--- 基础版本/tools.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path')
-var ToolsContainerDirName = './tools/tools.json'
-var ToolsContainer = require(ToolsContainerDirName);
-ToolsContainer.toolsPath = path.dirname(ToolsContainerDirName);
-ToolsContainer.getDependencies = function(dependenciesName){
-    return path.resolve(__dirname, ToolsContainer.toolsPath, ToolsContainer.dependencies[dependenciesName])
-}
-
-module.exports = ToolsContainer
