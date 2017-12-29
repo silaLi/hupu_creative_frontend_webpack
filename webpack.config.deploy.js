6fb5a69c05577f1367e79b43d51cc6e10c8ff2aa
李杨 <liyang@liyangdeMacBook-Pro.local>
2017-12-29 10:41:05 +0800

    dep config

--- webpack.config.deploy.js
+++ webpack.config.deploy.js
@@ -1,15 +1,11 @@
 var ToolsContainer = require('./tools/tools.config.js');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
-var SftpWebpackPlugin = require('sftp-webpack-plugin')
 var path = require('path')
-
-var private_config = './private_config/private.config.json'
-private_config = require(private_config);
 
 module.exports = {
     entry: {
         // index: './front-src/entry/index.ts',
-        index: path.resolve(__dirname + '/./front-src/entry/index.test.ts'),
+        index: path.resolve(__dirname + '/./front-src/entry/index.ts'),
     },
     output: {
         path: path.resolve(__dirname + '/./dest/deploy/'), // 输出文件的保存路径
@@ -42,23 +38,5 @@ module.exports = {
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js' ]
-    },
-    plugins: [
-        new CleanWebpackPlugin([path.resolve(__dirname + '/./dest/deploy/')], {
-            root: '', // An absolute path for the root  of webpack.config.js
-            verbose: true, // Write logs to console.
-            dry: false // Do not delete anything, good for testing.
-        }),
-        new SftpWebpackPlugin({
-            port: private_config.SftpWebpackPlugin.port,
-            host: private_config.SftpWebpackPlugin.host,
-            username: private_config.SftpWebpackPlugin.username,
-            password: private_config.SftpWebpackPlugin.password,
-            from: path.resolve(__dirname + '/./dest/deploy/'),
-            to: private_config.SftpWebpackPlugin.to
-        })
-    ],
-    externals: {
-
     }
 }
